249459d6f2212185f634bc3070bf9e98e2d08af2
diego939 <diegodavidalmiron1990@gmail.com>
2025-07-12 20:15:36 -0300

    fix change color pdf

--- src/routes/educacion/+page.svelte
+++ src/routes/educacion/+page.svelte
@@ -323,7 +323,7 @@
 									{#if cert.pdf}
 										<button 
 											on:click={() => abrirPDF(cert.pdf)}
-											class="flex-1 bg-red-600 text-white text-xs py-2 px-3 rounded hover:bg-blue-700 transition-colors flex items-center justify-center gap-1"
+											class="flex-1 bg-red-600 text-white text-xs py-2 px-3 rounded hover:bg-red-700 transition-colors flex items-center justify-center gap-1"
 										>
 											<svg class="w-3 h-3" fill="none" stroke="currentColor" viewBox="0 0 24 24">
 												<path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 10v6m0 0l-3-3m3 3l3-3m2 8H7a2 2 0 01-2-2V5a2 2 0 012-2h5.586a1 1 0 01.707.293l5.414 5.414a1 1 0 01.293.707V19a2 2 0 01-2 2z"></path>
